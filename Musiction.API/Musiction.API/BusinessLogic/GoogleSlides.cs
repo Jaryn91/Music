7506c04e7516c83d263242513cdf8d8a985a7810
Tomasz Jarzynski <jarzynski.tomasz@gmail.com>
2018-10-19 16:49:38 +0200

    Google slides first commit

---
+++ Musiction.API/Musiction.API/BusinessLogic/GoogleSlides.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Musiction.API.BusinessLogic
+{
+    public class GoogleSlides
+    {
+        public void Download(int id)
+        {
+
+        }
+    }
+}
